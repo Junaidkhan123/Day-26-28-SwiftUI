e6fcf59d7da63e857b2030d80b66c33f8f024e26
Muhammad  Junaid <junaid123.jk78@gmail.com>
2020-07-04 20:58:19 +0500

    Initial Commit

---
+++ BetterRest-26th-DaySwift/BetterRest_26th_DaySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRest_26th_DaySwiftApp.swift
+//  BetterRest-26th-DaySwift
+//
+//  Created by Jhon Khan on 04/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct BetterRest_26th_DaySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
