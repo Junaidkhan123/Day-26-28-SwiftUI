ce48965dda0104c87fe6a05fe2f178636e42d601
Muhammad  Junaid <junaid123.jk78@gmail.com>
2020-07-05 14:03:01 +0500

    Completed Day 26, 27 and 28

--- BetterRest-26th-DaySwift/ContentView.swift
+++ BetterRest-26th-DaySwift/ContentView.swift
@@ -8,8 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    @State private var wakeUp     = defaultWakeTime
+    @State private var coffeeAmount = 0
+    
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        
+        NavigationView {
+            
+            Form {
+                
+                Section(header:Text("When to do you want to wake up?")) {
+                    DatePicker("Select Date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                Section(header:  Text("Desired Amount of Sleep?")) {
+                    Picker(coffeeAmount == 0 ? "1 cup" : "\(coffeeAmount) cups", selection: $coffeeAmount) {
+                        ForEach(1..<21) {number in
+                           Text("\(number) cups")
+                        }
+                    }
+                }
+                
+                Section(header:  Text("Desired Amount of Sleep?")) {
+                    Stepper(value: $sleepAmount, in: 4...12,step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g") hours")
+                        
+                    }
+                }
+                
+                Section(header: Text("Your Idea bed Time:")) {
+                    Text("\(calculatSleep)")
+                        .font(.largeTitle)
+                }
+            }
+            .navigationTitle("Better Rest")
+        }
+    }
+    
+    
+     var calculatSleep : String {
+        
+        let model = SleepCalculator()
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        var msg    = ""
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            
+            msg = formatter.string(from: sleepTime)
+            
+        } catch {
+            msg = "Sorry, there was a problem calculating your bedtime."
+        }
+        return msg
+    }
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date()
     }
 }
 
